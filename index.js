2d59cef857c7702ccf11f0053e1ad4fd17bff1cc
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 21:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function theBeatlesPlay (musicianArray, instrumentArray) {
   var musicianInstrumentPair = [];
   for (let i = 0; i < musicianArray.length; i++) {
-    musicianInstrumentPair.push(`${musician} plays ${instrumentArray}`);
+    musicianInstrumentPair.push(`${musician[i]} plays ${instrumentArray[i]}`);
   }
   return musicianInstrumentPair;
 }
 
 function johnLennonFacts (factsArray) {
-  
+  while ()
 }
